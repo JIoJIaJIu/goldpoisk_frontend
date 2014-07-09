4ebef9bd248d76bf1735c06516bd737d03d64ed7
YLeight <yleight@gmail.com>
2014-07-09 07:01:39 +0400

    Блок логотипа

---
+++ desktop.blocks/g-logo/g-logo.js
@@ -0,0 +1,14 @@
+/*modules.define('i-bem__dom', function(provide, DOM) {
+    DOM.decl('g-logo', {
+        onSetMod: {
+            'color-theme' : {
+                'xmas' : function() {
+                   alert('xmas');            
+                },
+                '' : function() {
+
+                }
+            }
+        }
+    })
+}*/
