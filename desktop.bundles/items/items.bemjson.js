3c9cc9cb9f24e093fe3c397741959bfa86161c0c
YLeight <yleight@gmail.com>
2014-08-13 23:00:31 +0400

    Страница с итемами

---
+++ desktop.bundles/items/items.bemjson.js
@@ -0,0 +1,165 @@
+({
+    block: 'page',
+    title: 'items',
+    head: [
+        { elem: 'css', url: '_items.css' },
+    ],
+    scripts: [{ elem: 'js', url: '_items.js' }],
+    content: [
+        {
+        block: 'g-header',
+        content: [
+            {
+                elem: 'logo',
+                content: [
+                    {
+                        block: 'g-logo',
+                        description: 'Крупнейший поиск ювелирных изделий'
+                    }
+                ]
+            },
+            {
+                elem: 'basket',
+                content: [
+                    {
+                        tag: 'img',
+                        attrs: {src: '../../desktop.blocks/g-header/cart.png'},
+                    },
+                    {
+                        tag: 'span',
+                        content: 'Карзина пуста. Ищите товары в каталоге'
+                    }
+                ]
+            },
+            {
+                elem: 'desire',
+                content: [
+                    {
+                        tag: 'img',
+                        attrs: {src: '../../desktop.blocks/g-header/heart.png'},
+                    },
+                    {
+                        tag: 'span',
+                        content: 'Нет товаров в списке желаний'
+                    }
+                ] 
+            },
+            {
+                block: 'g-support',
+                content: [
+                ]
+            },
+            {
+                elem: 'clear',
+            },
+        ]},
+        {
+            block: 'g-category',
+            content: [
+                {
+                    block: 'g-item',
+                    mods: { type: 'necklaces' },
+                    label: 'КОЛЬЕ'
+                },
+                {
+                    block: 'g-item',
+                    mods: { type: 'chains' },
+                    label: 'ЦЕПИ'
+                },
+                {
+                    block: 'g-item',
+                    mods: { type: 'pendants' },
+                    label: 'ПОДВЕСКИ'
+                },
+                {
+                    block: 'g-item',
+                    mods: { type: 'bracelets' },
+                    label: 'БРАСЛЕТЫ'
+                },
+                {
+                    block: 'g-item',
+                    mods: { type: 'rings' },
+                    label: 'КОЛЬЦА'
+                },
+                {
+                    block: 'g-item',
+                    mods: { type: 'earrings' },
+                    label: 'СЕРЬГИ'
+                },
+                {
+                    block: 'g-item',
+                    mods: { type: 'brooches' },
+                    label: 'БРОШИ<br>И ЗАЖИМЫ'
+                },
+                {
+                    block: 'g-item',
+                    mods: { type: 'watches' },
+                    label: 'ЧАСЫ'
+                },
+                {
+                    block: 'g-item',
+                    mods: { type: 'cutlery' },
+                    label: 'СТОЛОВЫЕ<br>ПРИБОРЫ'
+                }
+            ]
+        },
+        {
+            block: 'g-content',
+            content: [
+                {
+                    block: 'g-filter',
+                    content: [
+                        {
+                            elem: 'title',
+                            title: 'Поиск по параетрам',
+                        },
+                        {
+                            elem: 'price',
+                        },
+                        {
+                            elem: 'part',
+                            mods: { type: 'material' },
+                            types: ['Золото', 'Серебро', 'Платина', 'Бижутерия'],
+                            title: 'Материал'
+                        },
+                        {
+                            elem: 'part',
+                            mods: { type: 'with-stones' },
+                            types: ['С бриллиантами', 'С сапфирами', 'С рубинами', 'C изумрудами'],
+                            title: 'С камнями'
+                                
+                        },
+                        {
+                            elem: 'part',
+                            mods: { type: 'size' },
+                            types: [],
+                            title: 'Размер'
+                        },
+                        {
+                            elem: 'part',
+                            mods: { type: 'weight' },
+                            types: [],
+                            title: 'Вес'
+                        },
+                        {
+                            elem: 'part',
+                            mods: { type: 'shop' },
+                            types: [],
+                            title: 'Магазин'
+                        },
+                        {
+                            elem: 'hide-button',
+                            content: ""
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            block: 'g-footer',
+            content: [
+
+            ]
+        }
+    ]
+})
---
+++ desktop.bundles/items/items.deps.js
@@ -0,0 +1,720 @@
+exports.deps = [
+    {
+        "block": "i-bem"
+    },
+    {
+        "block": "i-bem",
+        "elem": "internal"
+    },
+    {
+        "block": "inherit"
+    },
+    {
+        "block": "identify"
+    },
+    {
+        "block": "next-tick"
+    },
+    {
+        "block": "objects"
+    },
+    {
+        "block": "functions"
+    },
+    {
+        "block": "events"
+    },
+    {
+        "block": "page"
+    },
+    {
+        "block": "i-bem",
+        "elem": "dom"
+    },
+    {
+        "block": "jquery"
+    },
+    {
+        "block": "loader"
+    },
+    {
+        "block": "loader",
+        "mod": "type"
+    },
+    {
+        "block": "loader",
+        "mod": "type",
+        "val": "js"
+    },
+    {
+        "block": "jquery",
+        "elem": "config"
+    },
+    {
+        "block": "ua"
+    },
+    {
+        "block": "dom"
+    },
+    {
+        "block": "i-bem",
+        "elem": "dom",
+        "mod": "init"
+    },
+    {
+        "block": "i-bem",
+        "elem": "dom",
+        "mod": "init",
+        "val": "auto"
+    },
+    {
+        "block": "page",
+        "elem": "css"
+    },
+    {
+        "block": "page",
+        "elem": "js"
+    },
+    {
+        "block": "g-header"
+    },
+    {
+        "block": "g-header",
+        "elem": "logo"
+    },
+    {
+        "block": "g-header",
+        "elem": "basket"
+    },
+    {
+        "block": "g-header",
+        "elem": "desire"
+    },
+    {
+        "block": "g-header",
+        "elem": "clear"
+    },
+    {
+        "block": "g-logo"
+    },
+    {
+        "block": "g-support"
+    },
+    {
+        "block": "g-category"
+    },
+    {
+        "block": "g-item"
+    },
+    {
+        "block": "g-item",
+        "mod": "type"
+    },
+    {
+        "block": "g-item",
+        "mod": "type",
+        "val": "necklaces"
+    },
+    {
+        "block": "g-item",
+        "mod": "type",
+        "val": "chains"
+    },
+    {
+        "block": "g-item",
+        "mod": "type",
+        "val": "pendants"
+    },
+    {
+        "block": "g-item",
+        "mod": "type",
+        "val": "bracelets"
+    },
+    {
+        "block": "g-item",
+        "mod": "type",
+        "val": "rings"
+    },
+    {
+        "block": "g-item",
+        "mod": "type",
+        "val": "earrings"
+    },
+    {
+        "block": "g-item",
+        "mod": "type",
+        "val": "brooches"
+    },
+    {
+        "block": "g-item",
+        "mod": "type",
+        "val": "watches"
+    },
+    {
+        "block": "g-item",
+        "mod": "type",
+        "val": "cutlery"
+    },
+    {
+        "block": "g-content"
+    },
+    {
+        "block": "g-checkbox"
+    },
+    {
+        "block": "link"
+    },
+    {
+        "block": "link",
+        "mod": "disabled"
+    },
+    {
+        "block": "jquery",
+        "elem": "event"
+    },
+    {
+        "block": "jquery",
+        "elem": "event",
+        "mod": "type"
+    },
+    {
+        "block": "jquery",
+        "elem": "event",
+        "mod": "type",
+        "val": "pointer"
+    },
+    {
+        "block": "jquery",
+        "elem": "event",
+        "mod": "type",
+        "val": "pointerclick"
+    },
+    {
+        "block": "jquery",
+        "elem": "event",
+        "mod": "type",
+        "val": "pointerpressrelease"
+    },
+    {
+        "block": "base-control"
+    },
+    {
+        "block": "g-filter"
+    },
+    {
+        "block": "g-filter",
+        "elem": "title"
+    },
+    {
+        "block": "g-filter",
+        "elem": "price"
+    },
+    {
+        "block": "g-filter",
+        "elem": "part"
+    },
+    {
+        "block": "g-filter",
+        "elem": "hide-button"
+    },
+    {
+        "block": "g-filter",
+        "mod": "type"
+    },
+    {
+        "block": "g-filter",
+        "mod": "type",
+        "val": "material"
+    },
+    {
+        "block": "g-filter",
+        "mod": "type",
+        "val": "with-stones"
+    },
+    {
+        "block": "g-filter",
+        "mod": "type",
+        "val": "size"
+    },
+    {
+        "block": "g-filter",
+        "mod": "type",
+        "val": "weight"
+    },
+    {
+        "block": "g-filter",
+        "mod": "type",
+        "val": "shop"
+    },
+    {
+        "block": "g-footer"
+    }
+];
+exports.depsByTechs = {
+    "": {},
+    "spec.js": {
+        "bemhtml": [
+            {
+                "tech": "bemhtml",
+                "block": "i-bem"
+            },
+            {
+                "tech": "bemhtml",
+                "block": "i-bem",
+                "elem": "internal"
+            },
+            {
+                "tech": "bemhtml",
+                "block": "inherit"
+            },
+            {
+                "tech": "bemhtml",
+                "block": "identify"
+            },
+            {
+                "tech": "bemhtml",
+                "block": "next-tick"
+            },
+            {
+                "tech": "bemhtml",
+                "block": "objects"
+            },
+            {
+                "tech": "bemhtml",
+                "block": "functions"
+            },
+            {
+                "tech": "bemhtml",
+                "block": "events"
+            },
+            {
+                "tech": "bemhtml",
+                "block": "i-bem",
+                "elem": "dom"
+            },
+            {
+                "tech": "bemhtml",
+                "block": "jquery"
+            },
+            {
+                "tech": "bemhtml",
+                "block": "loader"
+            },
+            {
+                "tech": "bemhtml",
+                "block": "loader",
+                "mod": "type"
+            },
+            {
+                "tech": "bemhtml",
+                "block": "loader",
+                "mod": "type",
+                "val": "js"
+            },
+            {
+                "tech": "bemhtml",
+                "block": "jquery",
+                "elem": "config"
+            },
+            {
+                "tech": "bemhtml",
+                "block": "ua"
+            },
+            {
+                "tech": "bemhtml",
+                "block": "dom"
+            },
+            {
+                "tech": "bemhtml",
+                "block": "i-bem",
+                "elem": "dom",
+                "mod": "init"
+            },
+            {
+                "tech": "bemhtml",
+                "block": "link"
+            },
+            {
+                "tech": "bemhtml",
+                "block": "link",
+                "mod": "disabled"
+            },
+            {
+                "tech": "bemhtml",
+                "block": "jquery",
+                "elem": "event"
+            },
+            {
+                "tech": "bemhtml",
+                "block": "jquery",
+                "elem": "event",
+                "mod": "type"
+            },
+            {
+                "tech": "bemhtml",
+                "block": "jquery",
+                "elem": "event",
+                "mod": "type",
+                "val": "pointer"
+            },
+            {
+                "tech": "bemhtml",
+                "block": "jquery",
+                "elem": "event",
+                "mod": "type",
+                "val": "pointerclick"
+            },
+            {
+                "tech": "bemhtml",
+                "block": "jquery",
+                "elem": "event",
+                "mod": "type",
+                "val": "pointerpressrelease"
+            },
+            {
+                "tech": "bemhtml",
+                "block": "base-control"
+            }
+        ],
+        "spec.js": [
+            {
+                "tech": "spec.js",
+                "block": "i-bem",
+                "elem": "dom"
+            },
+            {
+                "tech": "spec.js",
+                "block": "jquery"
+            },
+            {
+                "tech": "spec.js",
+                "block": "loader"
+            },
+            {
+                "tech": "spec.js",
+                "block": "loader",
+                "mod": "type"
+            },
+            {
+                "tech": "spec.js",
+                "block": "loader",
+                "mod": "type",
+                "val": "js"
+            },
+            {
+                "tech": "spec.js",
+                "block": "jquery",
+                "elem": "config"
+            },
+            {
+                "tech": "spec.js",
+                "block": "ua"
+            },
+            {
+                "tech": "spec.js",
+                "block": "objects"
+            },
+            {
+                "tech": "spec.js",
+                "block": "functions"
+            },
+            {
+                "tech": "spec.js",
+                "block": "dom"
+            },
+            {
+                "tech": "spec.js",
+                "block": "i-bem",
+                "elem": "dom",
+                "mod": "init"
+            },
+            {
+                "tech": "spec.js",
+                "block": "i-bem"
+            },
+            {
+                "tech": "spec.js",
+                "block": "i-bem",
+                "elem": "internal"
+            },
+            {
+                "tech": "spec.js",
+                "block": "inherit"
+            },
+            {
+                "tech": "spec.js",
+                "block": "identify"
+            },
+            {
+                "tech": "spec.js",
+                "block": "next-tick"
+            },
+            {
+                "tech": "spec.js",
+                "block": "events"
+            },
+            {
+                "tech": "spec.js",
+                "block": "i-bem",
+                "elem": "dom"
+            },
+            {
+                "tech": "spec.js",
+                "block": "link"
+            },
+            {
+                "tech": "spec.js",
+                "block": "link",
+                "mod": "disabled"
+            },
+            {
+                "tech": "spec.js",
+                "block": "jquery",
+                "elem": "event"
+            },
+            {
+                "tech": "spec.js",
+                "block": "jquery",
+                "elem": "event",
+                "mod": "type"
+            },
+            {
+                "tech": "spec.js",
+                "block": "jquery",
+                "elem": "event",
+                "mod": "type",
+                "val": "pointer"
+            },
+            {
+                "tech": "spec.js",
+                "block": "jquery",
+                "elem": "event",
+                "mod": "type",
+                "val": "pointerclick"
+            },
+            {
+                "tech": "spec.js",
+                "block": "jquery",
+                "elem": "event",
+                "mod": "type",
+                "val": "pointerpressrelease"
+            },
+            {
+                "tech": "spec.js",
+                "block": "base-control"
+            },
+            {
+                "tech": "spec.js",
+                "block": "base-control"
+            },
+            {
+                "tech": "spec.js",
+                "block": "jquery"
+            },
+            {
+                "tech": "spec.js",
+                "block": "dom"
+            },
+            {
+                "tech": "spec.js",
+                "block": "i-bem"
+            },
+            {
+                "tech": "spec.js",
+                "block": "jquery",
+                "elem": "event",
+                "mod": "type",
+                "val": "pointer"
+            },
+            {
+                "tech": "spec.js",
+                "block": "jquery",
+                "elem": "config"
+            },
+            {
+                "tech": "spec.js",
+                "block": "events"
+            },
+            {
+                "tech": "spec.js",
+                "block": "jquery",
+                "elem": "event",
+                "mod": "type",
+                "val": "pointerpressrelease"
+            }
+        ]
+    },
+    "bemhtml": {
+        "bemhtml": [
+            {
+                "tech": "bemhtml",
+                "block": "i-bem"
+            },
+            {
+                "tech": "bemhtml",
+                "block": "i-bem",
+                "elem": "internal"
+            },
+            {
+                "tech": "bemhtml",
+                "block": "inherit"
+            },
+            {
+                "tech": "bemhtml",
+                "block": "identify"
+            },
+            {
+                "tech": "bemhtml",
+                "block": "next-tick"
+            },
+            {
+                "tech": "bemhtml",
+                "block": "objects"
+            },
+            {
+                "tech": "bemhtml",
+                "block": "functions"
+            },
+            {
+                "tech": "bemhtml",
+                "block": "events"
+            },
+            {
+                "tech": "bemhtml",
+                "block": "ua"
+            },
+            {
+                "tech": "bemhtml",
+                "block": "i-bem"
+            },
+            {
+                "tech": "bemhtml",
+                "block": "i-bem",
+                "elem": "dom"
+            },
+            {
+                "tech": "bemhtml",
+                "block": "jquery"
+            },
+            {
+                "tech": "bemhtml",
+                "block": "loader"
+            },
+            {
+                "tech": "bemhtml",
+                "block": "loader",
+                "mod": "type"
+            },
+            {
+                "tech": "bemhtml",
+                "block": "loader",
+                "mod": "type",
+                "val": "js"
+            },
+            {
+                "tech": "bemhtml",
+                "block": "jquery",
+                "elem": "config"
+            },
+            {
+                "tech": "bemhtml",
+                "block": "ua"
+            },
+            {
+                "tech": "bemhtml",
+                "block": "dom"
+            },
+            {
+                "tech": "bemhtml",
+                "block": "i-bem",
+                "elem": "dom",
+                "mod": "init"
+            },
+            {
+                "tech": "bemhtml",
+                "block": "link"
+            },
+            {
+                "tech": "bemhtml",
+                "block": "link",
+                "mod": "disabled"
+            },
+            {
+                "tech": "bemhtml",
+                "block": "jquery",
+                "elem": "event"
+            },
+            {
+                "tech": "bemhtml",
+                "block": "jquery",
+                "elem": "event",
+                "mod": "type"
+            },
+            {
+                "tech": "bemhtml",
+                "block": "jquery",
+                "elem": "event",
+                "mod": "type",
+                "val": "pointer"
+            },
+            {
+                "tech": "bemhtml",
+                "block": "jquery",
+                "elem": "event",
+                "mod": "type",
+                "val": "pointerclick"
+            },
+            {
+                "tech": "bemhtml",
+                "block": "jquery",
+                "elem": "event",
+                "mod": "type",
+                "val": "pointerpressrelease"
+            },
+            {
+                "tech": "bemhtml",
+                "block": "base-control"
+            },
+            {
+                "tech": "bemhtml",
+                "block": "events"
+            },
+            {
+                "tech": "bemhtml",
+                "block": "i-bem",
+                "elem": "dom"
+            },
+            {
+                "tech": "bemhtml",
+                "block": "jquery",
+                "elem": "event",
+                "mod": "type",
+                "val": "pointer"
+            },
+            {
+                "tech": "bemhtml",
+                "block": "base-control"
+            },
+            {
+                "tech": "bemhtml",
+                "block": "jquery"
+            },
+            {
+                "tech": "bemhtml",
+                "block": "dom"
+            },
+            {
+                "tech": "bemhtml",
+                "block": "jquery",
+                "elem": "event",
+                "mod": "type",
+                "val": "pointerpressrelease"
+            },
+            {
+                "tech": "bemhtml",
+                "block": "jquery",
+                "elem": "config"
+            }
+        ]
+    }
+};
