50310e0a592ac749abe25db1263279656428add3
Guro Bokum <jiojiajiu@gmail.com>
2014-11-02 18:11:48 +0400

    Update compiling

--- compile.sh
+++ compile.sh
@@ -4,4 +4,4 @@ npm install
 ./node_modules/bem/bin/bem make
 
 echo "Borschik"
-borschik --input=desktop.bundles/index/_index.css --minimize=no  --output=desktop.bundles/index/index.css 
+./node_modules/borschik/bin/borschik --input=desktop.bundles/index/_index.css --minimize=no  --output=desktop.bundles/index/index.css 
