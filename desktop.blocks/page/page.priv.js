9771a5269245815842114ac940652795380879ea
Guro Bokum <jiojiajiu@gmail.com>
2014-11-30 18:17:32 +0300

    Fix page.priv

--- desktop.blocks/page/page.priv.js
+++ desktop.blocks/page/page.priv.js
@@ -8,7 +8,7 @@ blocks['page'] = function (data, env) {
             { elem: 'meta', attrs: { name: 'description', content: '' } },
             { elem: 'meta', attrs: { name: 'viewport', content: 'width=device-width, initial-scale=1' } },
             { elem: 'css', url:  'css/index.css' }
-        ),
+        ],
         scripts: [{ elem: 'js', url: '_index.js' }],
         mods: { theme: 'normal' },
         content: []
