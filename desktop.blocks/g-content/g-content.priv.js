32f552e41edfaff1bc84e67000d8bd5a0eb5a704
Guro Bokum <jiojiajiu@gmail.com>
2014-12-02 13:00:16 +0300

    index priv

--- desktop.blocks/g-content/g-content.priv.js
+++ desktop.blocks/g-content/g-content.priv.js
@@ -5,7 +5,34 @@ blocks['g-content.index'] = function (data, env) {
             {
                 block: 'fotorama',
                 src: data.promo[0]
+            }, {
+                block: 'g-pride',
+                content: [
+                    {
+                        block: 'g-pride-item',
+                        mods: { type: 'brown' },
+                        label: 'Лучшие производители ювелирных изделий'
+                    },
+                    {
+                        block: 'g-pride-item',
+                        mods: { type: 'beige' },
+                        label: 'Огромный ассортимент представлен в каталоге'
+                    },
+                    {
+                        block: 'g-pride-item',
+                        mods: { type: 'gold' },
+                        label: 'Только проверенные партнёры и предложения'
+                    },
+                    {
+                        block: 'g-pride-item',
+                        mods: { type: 'gray' },
+                        label: 'Простой и удобный поиск по параметрам'
+                    }
+                ]
             }
         ]
+    }, {
+        block: 'g-goods',
+        goods: (data.products) ? JSON.parse(data.products) : []
     }]
 }
