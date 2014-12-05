c0c7fe9dbef7b7c0a8f926edb58f7a14a52c9187
Guro Bokum <jiojiajiu@gmail.com>
2014-12-05 21:19:36 +0300

    favicon.ico

--- desktop.blocks/page/page.priv.js
+++ desktop.blocks/page/page.priv.js
@@ -3,7 +3,7 @@ blocks['page'] = function (data, env) {
     return {
         block: 'page',
         title: 'GoldPoisk',
-        favicon: '/favicon.ico',
+        favicon: '/media/favicon.ico',
         head: [
             { elem: 'meta', attrs: { name: 'description', content: '' } },
             { elem: 'meta', attrs: { name: 'viewport', content: 'width=device-width, initial-scale=1' } },
