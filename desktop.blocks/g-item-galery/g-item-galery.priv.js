2ba18ea4dea5b8d7037e9999ca0c05fc3baa6d6d
YLeight <yleight@gmail.com>
2014-12-06 02:53:01 +0300

    Хлебные крошки + расширение галереи + стили контента + блок clear для плавающих элементов

---
+++ desktop.blocks/g-item-galery/g-item-galery.priv.js
@@ -0,0 +1,26 @@
+blocks['g-item-galery'] = function (data, env) {
+	/**
+	 *		@key {Number} [index] default = 0
+	 *		@key {Array} images
+	 **/
+	 var MAX_IMG_COUNTS = 4;
+	 return [
+	 	block: 'g-item-galery',
+	 	content: [
+	 		{
+	 			elem: 'title',
+	 			title: 'Фото',
+	 			current: data.index,
+	 			count: data.images.length() > MAX_IMG_COUNTS ? MAX_IMG_COUNTS : data.images.length();
+	 		},
+	 		{
+	 			elem: 'preview_images',
+	 			images: data.images.length() > MAX_IMG_COUNTS ? data.images.slice(0, MAX_IMG_COUNTS) : data.images;
+	 		},
+	 		{
+	 			elem: 'main_image',
+	 			image: data.images[index]
+	 		}
+	 	]
+	 ]
+}
