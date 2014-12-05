2a91845832a45dc8a8b540d0d0dbe1ede21c1170
Guro Bokum <jiojiajiu@gmail.com>
2014-12-05 14:37:27 +0300

    fix index.priv

--- desktop.blocks/g-content/g-content.priv.js
+++ desktop.blocks/g-content/g-content.priv.js
@@ -1,38 +1,36 @@
 blocks['g-content.index'] = function (data, env) {
     return [{
         block: 'g-promotion',
-        content: [
-            {
+        content: {
                 block: 'fotorama',
                 src: data.promo[0]
-            }, {
-                block: 'g-pride',
-                content: [
-                    {
-                        block: 'g-pride-item',
-                        mods: { type: 'brown' },
-                        label: 'Лучшие производители ювелирных изделий'
-                    },
-                    {
-                        block: 'g-pride-item',
-                        mods: { type: 'beige' },
-                        label: 'Огромный ассортимент представлен в каталоге'
-                    },
-                    {
-                        block: 'g-pride-item',
-                        mods: { type: 'gold' },
-                        label: 'Только проверенные партнёры и предложения'
-                    },
-                    {
-                        block: 'g-pride-item',
-                        mods: { type: 'gray' },
-                        label: 'Простой и удобный поиск по параметрам'
-                    }
-                ]
+        }
+    }, {
+        block: 'g-pride',
+        content: [
+            {
+                block: 'g-pride-item',
+                mods: { type: 'brown' },
+                label: 'Лучшие производители ювелирных изделий'
+            },
+            {
+                block: 'g-pride-item',
+                mods: { type: 'beige' },
+                label: 'Огромный ассортимент представлен в каталоге'
+            },
+            {
+                block: 'g-pride-item',
+                mods: { type: 'gold' },
+                label: 'Только проверенные партнёры и предложения'
+            },
+            {
+                block: 'g-pride-item',
+                mods: { type: 'gray' },
+                label: 'Простой и удобный поиск по параметрам'
             }
         ]
     }, {
         block: 'g-goods',
-        goods: (data.products) ? JSON.parse(data.products) : []
+        content: (data.products) ? JSON.parse(data.products) : []
     }]
 }
