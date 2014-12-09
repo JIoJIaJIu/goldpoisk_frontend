26f93ae1b3bd447c91b269a68ac5e7ee51877a23
YLeight <yleight@gmail.com>
2014-12-09 23:00:49 +0300

    items.bemjson.js с составляющими блоками

--- desktop.bundles/items/items.bemjson.js
+++ desktop.bundles/items/items.bemjson.js
@@ -7,225 +7,150 @@
     scripts: [{ elem: 'js', url: '_items.js' }],
     content: [
         {
-        block: 'g-header',
-        content: [
-            {
-                elem: 'logo',
-                content: [
-                    {
-                        block: 'g-logo',
-                        description: 'Крупнейший поиск ювелирных изделий'
-                    }
-                ]
-            },
-            {
-                elem: 'basket',
-                content: [
-                    {
-                        tag: 'img',
-                        attrs: {src: '../../desktop.blocks/g-header/cart.png'},
-                    },
-                    {
-                        tag: 'span',
-                        content: 'Карзина пуста. Ищите товары в каталоге'
-                    }
-                ]
-            },
-            {
-                elem: 'desire',
-                content: [
-                    {
-                        tag: 'img',
-                        attrs: {src: '../../desktop.blocks/g-header/heart.png'},
-                    },
-                    {
-                        tag: 'span',
-                        content: 'Нет товаров в списке желаний'
-                    }
-                ] 
-            },
-            {
-                block: 'g-support',
-                content: [
-                ]
-            },
-            {
-                elem: 'clear',
-            },
-        ]},
-        {
-            block: 'g-category',
+            block: 'g-header',
             content: [
                 {
-                    block: 'g-item',
-                    mods: { type: 'necklaces' },
-                    label: 'КОЛЬЕ'
-                },
+                    block: 'g-logo',
+                    description: 'Крупнейший поиск ювелирных изделий'
+                }/*,
                 {
-                    block: 'g-item',
+                    block: 'g-basket',
+                    amount: 0
+                }/*,
+                {
+                    elem: 'desire',
+                    content: [
+                        {
+                            tag: 'img',
+                            attrs: {src: '../../desktop.blocks/g-header/heart.png'},
+                        },
+                        {
+                            tag: 'span',
+                            content: 'Нет товаров в списке желаний'
+                        }
+                    ] 
+                }*/,
+                {
+                    block: 'g-support',
+                    content: [
+                    ]
+                }, {
+                    elem: 'clear',
+                }
+            ] 
+        }, {
+            block: 'g-menu',
+            content: [
+                {
+                    block: 'g-menu-item',
+                    mods: { type: 'necklaces', state: 'active' },
+                    label: 'КОЛЬЕ'
+                }, {
+                    block: 'g-menu-item',
                     mods: { type: 'chains' },
                     label: 'ЦЕПИ'
-                },
-                {
-                    block: 'g-item',
+                }, {
+                    block: 'g-menu-item',
                     mods: { type: 'pendants' },
                     label: 'ПОДВЕСКИ'
-                },
-                {
-                    block: 'g-item',
+                }, {
+                    block: 'g-menu-item',
                     mods: { type: 'bracelets' },
                     label: 'БРАСЛЕТЫ'
-                },
-                {
-                    block: 'g-item',
+                }, {
+                    block: 'g-menu-item',
                     mods: { type: 'rings' },
                     label: 'КОЛЬЦА'
-                },
-                {
-                    block: 'g-item',
+                }, {
+                    block: 'g-menu-item',
                     mods: { type: 'earrings' },
                     label: 'СЕРЬГИ'
-                },
-                {
-                    block: 'g-item',
+                }, {
+                    block: 'g-menu-item',
                     mods: { type: 'brooches' },
                     label: 'БРОШИ<br>И ЗАЖИМЫ'
-                },
-                {
-                    block: 'g-item',
+                }, {
+                    block: 'g-menu-item',
                     mods: { type: 'watches' },
                     label: 'ЧАСЫ'
-                },
-                {
-                    block: 'g-item',
+                }, {
+                    block: 'g-menu-item',
                     mods: { type: 'cutlery' },
                     label: 'СТОЛОВЫЕ<br>ПРИБОРЫ'
                 }
             ]
-        },
-        {
+        }, {
             block: 'g-content',
             content: [
                 {
-                    block: 'g-navibar',
-                    root: 'Главная',
-                    position: 'Кольца'
-                },
-                {
+                    block: 'g-breadcrumbs',
+                    root: {
+                        title: 'Главная',
+                        url: '/'
+                    },
+                    path: [
+                        {
+                            title: 'Кольца',
+                            url: '/'
+                        }, {
+                            title: 'Обручальные',
+                            url: '/'
+                        }
+                    ]
+                }, {
                     block: 'g-category-title',
                     title: 'Кольца',
                     count: '11 253'
-                },
-                {
-                    block: 'g-filter',
-                    content: [
-                        {
-                            elem: 'title',
-                            title: 'Поиск по параетрам',
-                        },
-                        {
-                            elem: 'price',
-                        },
-                        {
-                            elem: 'part',
-                            mods: { type: 'material' },
-                            types: ['Золото', 'Серебро', 'Платина', 'Бижутерия'],
-                            title: 'Материал'
-                        },
-                        {
-                            elem: 'part',
-                            mods: { type: 'with-stones' },
-                            types: ['С бриллиантами', 'С сапфирами', 'С рубинами', 'C изумрудами'],
-                            title: 'С камнями'
-                                
-                        },
-                        {
-                            elem: 'part',
-                            mods: { type: 'size' },
-                            types: [],
-                            title: 'Размер'
-                        },
-                        {
-                            elem: 'part',
-                            mods: { type: 'weight' },
-                            types: [],
-                            title: 'Вес'
-                        },
-                        {
-                            elem: 'part',
-                            mods: { type: 'shop' },
-                            types: [],
-                            title: 'Магазин'
-                        },
-                        {
-                            elem: 'hide-button',
-                            content: ""
-                        }
-                    ]
-                },
-                {
+                }, {
                     block: 'g-goods',
-                    goods: [
-                        {
-                            title: 'Кольцо с 21 бриллиантом',
-                            image: '../../desktop.blocks/g-goods/good.png',
-                            pw: '4,5 грамм',
-                            jw: '0,07 карат',
-                            price: '3333Р',
-                            shop: 'Sunlight',
-                            action: true,
-                                                            
-                        },
-                        {
-                            hit: true,
-                            tape: 'Хит продаж'
-                        },
-                        {
-                            like: true,
-                        },
-                        {
-                        },
-                        {
-                            block: 'clear'
-                        }
-                    ]
-                },
-                {
-                    block: 'g-expanded-item',
                     content: [
                         {
-                            elem: 'title',
-                            title: 'Обручальное кольцо из белого золота с 21 бриллиантом'
-                        },
-                        {
-                            elem: 'close',
-                        },
-                        {
-                            block: 'g-item-galery',
-                            content: [
-                                {
-                                    elem: 'title',
-                                    title: 'Фото',
-                                    count: 8
-                                },
-                                {
-                                    elem: 'preview_images',
-                                    images: ['1.jpg',
-                                             '1.jpg',
-                                             '1.jpg',
-                                             '1.jpg']
-                                },
-                                {
-                                    elem: 'main_image',
-                                    image: '2.jpg'
-                                }
-                                
-                            ]
+                            block: 'g-product',
+                            mods: { like: 'yes', action: 'yes' },
+                            url: '#',
+                            title: 'Золотое Кольцо с гранатами и фианитами. Очень красивое и неебически дорогое',
+                            imageUrl: '../../desktop.blocks/g-goods/images/good.png',
+                            weight: '4.5 грамм',
+                            carat: '0,07 карат',
+                            price: 1000000,
+                            store: 'Sunlight',
+                            storeUrl: '#'
+                        }, {
+                            block: 'g-product',
+                            mods: { like: 'yes', action: 'yes' },
+                            url: '#',
+                            title: 'Золотое Кольцо с гранатами и фианитами. Очень красивое и неебически дорогое',
+                            imageUrl: '../../desktop.blocks/g-goods/images/good.png',
+                            weight: '4.5 грамм',
+                            carat: '0,07 карат',
+                            price: 1000000,
+                            store: 'Sunlight',
+                            storeUrl: '#'
+                        }, {
+                            block: 'g-product',
+                            mods: { like: 'yes', action: 'yes' },
+                            url: '#',
+                            title: 'Золотое Кольцо с гранатами и фианитами. Очень красивое и неебически дорогое',
+                            imageUrl: '../../desktop.blocks/g-goods/images/good.png',
+                            weight: '4.5 грамм',
+                            carat: '0,07 карат',
+                            price: 1000000,
+                            store: 'Sunlight',
+                            storeUrl: '#'
+                        }, {
+                            block: 'g-product',
+                            mods: { like: 'yes', action: 'yes' },
+                            url: '#',
+                            title: 'Золотое Кольцо с гранатами и фианитами. Очень красивое и неебически дорогое',
+                            imageUrl: '../../desktop.blocks/g-goods/images/good.png',
+                            weight: '4.5 грамм',
+                            carat: '0,07 карат',
+                            price: 1000000,
+                            store: 'Sunlight',
+                            storeUrl: '#'
                         }
                     ]
-                }
-                
-
+                }, 
             ]
         },
         {
