48ea91d0761f8caf8d60c3d21f699ec65c9d5825
Guro Bokum <jiojiajiu@gmail.com>
2014-12-24 07:27:42 +0300

    Removing server/

--- server/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    'init': function (server, express) {
-        server.use(express.logger());
-        server.use(express.static(__dirname + '/views/markup'));
-
-        server.engine('html', require('ejs').renderFile);
-    }
-}
