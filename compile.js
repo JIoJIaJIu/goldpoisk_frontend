1fdf16f250f7f2a0f613f0f639b268dadd208c98
Guro Bokum <jiojiajiu@gmail.com>
2015-02-10 14:20:20 +0700

    New compilation

---
+++ compile.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+
+var Q = require('q');
+var B = require('bem');
+var BEM = B.api;
+
+var levels = [
+    'bem-core/common.blocks',
+    'bem-core/desktop.blocks',
+    'min.blocks',
+    'desktop.blocks',
+];
+
+var techs = [
+    'node_modules/bem/lib/techs/v2/css.js',
+    '.bem/techs/priv.js.js',
+    './bem-core/.bem/techs/bemhtml.js',
+    './bem-core/.bem/techs/browser.js+bemhtml.js'
+];
+
+console.log('tech:', techs.join('\ntech: '));
+
+var promise = BEM.build({
+        outputDir: 'merge',
+        outputName: 'index',
+        outputLevel: B.createLevel('desktop.bundles/'),
+        declaration: 'desktop.bundles/merge/merge.deps.js',
+        level: levels,
+        tech: techs
+    });
+
+Q.when(promise, function () {
+        console.log('finished');
+    }, function (error) {
+        console.log('error', error);
+    }
+);
--- compile.sh
+++ compile.sh
@@ -15,25 +15,9 @@ bem decl merge \
     -d desktop.bundles/item/item.deps.js \
     -o desktop.bundles/merge/merge.deps.js
 
-LEVELS="-l bem-core/common.blocks \
-        -l bem-core/desktop.blocks \
-        -l min.blocks -l desktop.blocks"
-
-echo "Building css"
-bem build $LEVELS\
-    -d desktop.bundles/merge/merge.deps.js -T node_modules/bem/lib/techs/v2/css.js \
-    --force -o desktop.bundles/merge -n merge
-
-echo "Building bemhtml"
-bem build $LEVELS\
-    -d desktop.bundles/merge/merge.deps.js -T bem-core/.bem/techs/bemhtml.js \
-    --force -o desktop.bundles/merge -n merge
-
-echo "Building priv.js"
-bem build $LEVELS\
-    -d desktop.bundles/merge/merge.deps.js -T .bem/techs/priv.js.js \
-    -o desktop.bundles/merge -n merge
+echo "Compiling techs.."
+node compile.js
 
 echo "Borschik"
 mkdir -p desktop.bundles/merge/css
-./node_modules/borschik/bin/borschik --input=desktop.bundles/merge/merge.css --minimize=no  --output=desktop.bundles/merge/css/index.css
+./node_modules/borschik/bin/borschik --input=desktop.bundles/merge/index.css --minimize=no  --output=desktop.bundles/merge/css/index.css
