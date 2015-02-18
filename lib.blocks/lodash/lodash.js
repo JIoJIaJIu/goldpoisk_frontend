1c95c300c64d955d6c8cce31816d4fb11fc392b6
Guro Bokum <jiojiajiu@gmail.com>
2015-02-18 14:13:07 +0700

    lib.blocks via config

--- lib.blocks/lodash/lodash.js
+++ lib.blocks/lodash/lodash.js
@@ -1,5 +1,5 @@
-modules.define('slider', ['loader_type_js'], function (provide, loader) {
-    loader('http://goldpoisk.ru/js/third-parties/lodash.min.js', function () {
+modules.define('slider', ['loader_type_js', 'config'], function (provide, loader, config) {
+    loader(config.scripts.lodash, function () {
         provide(_);
     });
 });
--- lib.blocks/slider/slider.js
+++ lib.blocks/slider/slider.js
@@ -1,6 +1,5 @@
-modules.define('slider', ['loader_type_js', 'jquery'], function (provide, loader) {
-    //TODO:move to config
-    loader('http://goldpoisk.ru/js/third-parties/jssor.slider.min.js', function () {
+modules.define('slider', ['loader_type_js', 'config', 'jquery'], function (provide, loader, config) {
+    loader(config.scripts.slider, function () {
         provide($JssorSlider$);
     });
 });
