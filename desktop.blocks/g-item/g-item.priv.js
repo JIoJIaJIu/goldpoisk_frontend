23c18c82b2134d367e45fea14e72e2b134ae88a7
Guro Bokum <jiojiajiu@gmail.com>
2015-02-28 16:27:00 +0700

    g-item.priv

--- desktop.blocks/g-item/g-item.priv.js
+++ desktop.blocks/g-item/g-item.priv.js
@@ -1,11 +1,50 @@
-/** 
+/**
  * @param {String} title
  * @param {String} category
  * @param {Object} features
+ * @param {Object} [item]
  *
  */
 blocks['g-item'] = function (data, env) {
-    return {
+    var collumn = {
+        block: 'g-right-col',
+        content: [{
+                block: 'g-item-features',
+                content: data.features
+            }, {
+                block: 'g-item-description',
+                content: data.description
+            }, {
+                block : 'yashare',
+                quickServices : data.quickServices || [
+                    'vkontakte',
+                    'facebook',
+                    'twitter',
+                    'odnoklassniki'
+                ],
+                theme : data.theme || 'counter',
+                l10n : data.l10n || 'ru',
+                url : data.url,
+                title : data.likesTitle,
+                description : data.likesDescription,
+                image : 'https://raw.githubusercontent.com/voischev/bem-social/' +
+                        'master/desktop.bundles/index/blocks/page/image/bem.png'
+            }
+        ]
+    }
+
+    if (data.item) {
+        console.log(data.item);
+        collumn.content.unshift({
+            block: 'g-item-buy-in-shop',
+            shop: data.item.store,
+            shopUrl: data.item.storeUrl,
+            price: data.item.price,
+            buyUrl: data.item.buyUrl
+        });
+    }
+
+    var item =  {
         block: 'g-item',
         content: [
             {
@@ -18,42 +57,11 @@ blocks['g-item'] = function (data, env) {
             },
 
             blocks['g-item-gallery'](data.gallery, env),
-
-            {
-                block: 'g-right-col',
-                content: [
-                    {
-                        block: 'g-item-buy-in-shop',
-                        shop: data.shop,
-                        shopUrl: data.buyUrl,
-                        price: data.price,
-                        buyUrl: data.buyUrl
-                    }, {
-                        block: 'g-item-features',
-                        content: data.features
-                    }, {
-                        block: 'g-item-description',
-                        content: data.description
-                    }, {
-                        block : 'yashare',
-                        quickServices : data.quickServices || [
-                            'vkontakte',
-                            'facebook',
-                            'twitter',
-                            'odnoklassniki'
-                        ],
-                        theme : data.theme || 'counter',
-                        l10n : data.l10n || 'ru',
-                        url : data.url,
-                        title : data.likesTitle,
-                        description : data.likesDescription,
-                        image : 'https://raw.githubusercontent.com/voischev/bem-social/' +
-                                'master/desktop.bundles/index/blocks/page/image/bem.png'
-                    }
-                ]
-            }, {
+            collumn, {
                 block: 'clear'
             }
         ]
     }
+
+    return item
 }
