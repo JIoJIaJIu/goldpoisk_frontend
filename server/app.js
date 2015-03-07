66f7e8fc604a57918a1a9543c0c16823747a4ebf
Yegor Litvyakov <egor@emply.ru>
2015-03-07 19:27:12 +0700

    Pseudo server v1

---
+++ server/app.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var app = express();
+
+var product = {
+    block: 'g-product',
+    url: '#',
+    title: 'Новое кольцо',
+    imageUrl: '../../desktop.blocks/g-goods/images/good.png',
+    weight: '5.9 грамм',
+    carat: '0,14 карат',
+    price: 500,
+    store: 'GoldPoisk',
+    storeUrl: '#'
+}
+
+app.get('/success', function (req, res) {
+    res.json(product);
+});
+
+app.use( express.static('../') );
+
+var server = app.listen(3000, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('Example app listening at http://%s:%s', host, port);
+});
