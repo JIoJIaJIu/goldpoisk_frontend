f3a2039ca48edbeac163def2d55924de148abfa0
Yegor Litvyakov <egor@emply.ru>
2015-03-08 01:40:53 +0700

    g-sorting-goods block with js

---
+++ min.blocks/g-sorting-goods/g-sorting-goods.js
@@ -0,0 +1,23 @@
+modules.define('g-sorting-goods', ['i-bem__dom', 'jquery'], function(provide, BEMDOM, $) {
+    BEMDOM.decl('g-sorting-goods', {
+        _onParamClick: function(e) {
+            var goods = this.findBlockOutside('g-content').findBlockInside('g-goods');
+            var _target = $(e.currentTarget);
+            this.setMod(this.elem('param'), 'selected', false);
+            this.setMod(_target, 'selected', true);
+            $.getJSON('http://localhost:3000/sortparam', function sort (data) {
+                setTimeout(function () {
+                    goods.update(data);
+                })
+            })
+        }       
+    }, {
+        live: function () {
+            this.liveBindTo('param', 'click', function(e) {
+                this._onParamClick(e);
+            });
+            return false; // Если инициализация блока не может быть отложена
+        }
+    });
+    provide(BEMDOM)
+});
