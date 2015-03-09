d2c11c21276cda83db940bcb1d31f2b932d02ec1
Guro Bokum <jiojiajiu@gmail.com>
2015-03-10 00:35:12 +0700

    g-goods

--- desktop.blocks/g-goods/g-goods.priv.js
+++ desktop.blocks/g-goods/g-goods.priv.js
@@ -5,8 +5,24 @@ blocks['g-goods'] = function (data, env) {
         list.push(blocks['g-product'](item, env));
     }
 
-    return {
+    var block = {
         block: 'g-goods',
         content: list
     }
+
+    if (env && env.sorting)
+        block.mods = { 'sorting': true }
+
+    return block
+}
+
+//TODO;
+blocks['g-goods.str'] = function (data, env) {
+    list = JSON.parse(data)
+    for (var i = 0, length = list.length; i < length; i++) {
+        var item = list[i];
+        list.push(blocks['g-product'](item, env));
+    }
+
+    return list
 }
