263a2e7f8e1bc35047580ab6528c718be0fd837b
Guro Bokum <jiojiajiu@gmail.com>
2015-03-09 22:21:23 +0700

    g-product.priv.js

--- min.blocks/g-product/g-product.priv.js
+++ min.blocks/g-product/g-product.priv.js
@@ -1,26 +1,34 @@
 blocks['g-product'] = function (data, env) {
 	/**
 	 *		@param
-	 *		@key {string} url
 	 *		@key {String} title
-	 *		@key {Number} number
-	 *		@key {string} imageUrl
-	 *		@key {Array} images
-	 *		@key {string} weight
-	 *		@key {string} carat
-	 *		@key {Boolean} [newPage] open in new page or not
+	 *		@key {Number} number, article of item
+	 *		@key {String} url
+	 *		@key {String} image, url to main image
+	 *		@key {String} weight
+	 *		@key {String} carat
+     *		@key {Number} count, count of items
+	 *		@key {Boolean} [newPage] open in new page or not, default: true
      *		
 	 *		@key {bool} [action]
 	 *		@key {bool} [hit]
 	 *		@key {string} [tape]
 	 *		@key {bool} [like]
      *
-     *		@key {Object} [item]
-	 *		    @key {integer} price
-	 *		    @key {string} store
-	 *		    @key {string} storeUrl
-	 *		    @key {String} buyUrl buy url for item
+     *      @key {Array} items
+     *		    @of {Object} item
+	 *		        @key {integer} price
+	 *		        @key {string} store
+	 *		        @key {string} storeUrl
+	 *		        @key {String} buyUrl buy url for item
 	 **/
+    assertHas(data, 'title', 'Should be pointed title');
+    assertHas(data, 'number', 'Should be pointed number');
+    assertHas(data, 'url', 'Should be pointed url');
+    assertHas(data, 'image', 'Should be pointed image');
+    assertHas(data, 'weight', 'Should be pointed weight');
+    assertHas(data, 'count', 'Should be pointed count');
+
 	var block = {
 		block: 'g-product',
 		mods: {
@@ -30,21 +38,19 @@ blocks['g-product'] = function (data, env) {
 		},
 		url: data.url,
 		title: data.title,
-		imageUrl: data.imageUrl,
+		imageUrl: data.image,
 		weight: data.weight,
 		carat: data.carat,
-        newPage: data.newPage || false
+        newPage: data.newPage || true
 	}
 
-    if (data.item) {
-        block.item = {
-            price: data.price,
-            store: data.store,
-            storeUrl: data.storeUrl,
-            buyUrl: data.buyUrl || null
-        }
+    if (data.count == 1) {
+        block.price = data.minPrice;
+        block.storeUrl = data.shopUrl;
+        block.store = data.shopName;
+        block.buyUrl = data.buyUrl;
     }
-
+    /*
     if (env.js) {
         var features = [];
         block.js = {
@@ -68,6 +74,7 @@ blocks['g-product'] = function (data, env) {
                 'Карат': data.carat
             })
     }
+    */
 
     return block;
 }
