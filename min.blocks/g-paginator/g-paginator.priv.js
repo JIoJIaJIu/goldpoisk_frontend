3c6120e83e783a0a3a61baa4c3295e39c04a4cb1
Guro Bokum <jiojiajiu@gmail.com>
2015-03-10 00:36:02 +0700

    g-paginator/release

--- min.blocks/g-paginator/g-paginator.priv.js
+++ min.blocks/g-paginator/g-paginator.priv.js
@@ -4,12 +4,14 @@ blocks['g-paginator'] = function (data, env) {
     assertHas(data, 'totalPages', 'Should point totalPages for pagination');
     assertHas(data, 'currentPage', 'Should point currentPage');
     assertHas(data, 'url', 'Should point url');
+    assertHas(data, 'config', 'Should point config');
 
     return {
         block: 'g-paginator',
         totalPages: data.totalPages,
         currentPage: data.currentPage,
-        url: data.url
+        url: data.url,
+        config: data.config
     }
 }
 
