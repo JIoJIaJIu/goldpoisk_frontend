5a71eab47102185b7011072ce161c363e95254ad
Yegor Litvyakov <egor@emply.ru>
2015-03-16 00:45:04 +0700

    В начало - элемент шапки

--- desktop.blocks/g-header/g-header.js
+++ desktop.blocks/g-header/g-header.js
@@ -2,9 +2,17 @@ modules.define('g-header', ['i-bem__dom', 'jquery'], function(provide, BEMDOM, $
     BEMDOM.decl('g-header', {
         onSetMod: {
             'js': function () {
+
+                var inner = this.findBlockInside('g-header_inner');
+                inner.bindTo('up', 'click', function(e) {
+                    console.log('click');
+                    window.scrollTo(0, 0);
+                });
+
                 var self = this;
                 var support = this.findBlockInside('g-support');
                 var desc = $('.g-logo__description');
+
 
                 $(window).scroll(function(e) {
                     repos();
@@ -15,10 +23,12 @@ modules.define('g-header', ['i-bem__dom', 'jquery'], function(provide, BEMDOM, $
                         if ($(window).scrollTop() > 0) {
                             self.setMod('state', 'flow');
                             support.setMod('dark', true);
+                            $('.g-header_inner__up').css('display', 'block');
                             //desc.css('display', 'none');
                         } else {
                             self.delMod('state');
                             support.delMod('dark');
+                            $('.g-header_inner__up').css('display', 'none');
                             //desc.css('display', 'block');
                         }
                     });
