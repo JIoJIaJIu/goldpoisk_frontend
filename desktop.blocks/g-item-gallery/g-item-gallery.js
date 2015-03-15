c0b79e991469b5d44a1949f7577346bc1044557f
Guro Bokum <jiojiajiu@gmail.com>
2015-03-15 20:58:30 +0700

    Select/deselect gallery

--- desktop.blocks/g-item-gallery/g-item-gallery.js
+++ desktop.blocks/g-item-gallery/g-item-gallery.js
@@ -3,10 +3,16 @@ modules.define('g-item-gallery', ['i-bem__dom', 'jquery', 'slider'], function (p
         onSetMod: {
             js: function () {
                 var main = this.elem('main_image').find('img');
-                var thumbs = this.elem('preview_images');
+                var thumbs = this.findBlocksInside('preview_images', 'image');
                 //TODO: memory leaks
-                thumbs.bind('click', function (e) {
-                    main.attr('src', $(e.target).attr('src'));
+                thumbs.forEach(function (thumb) {
+                    thumb.bindTo('click', function (e) {
+                        thumbs.forEach(function (t) {
+                            t.delMod('state');
+                        });
+                        main.attr('src', thumb.params.src);
+                        thumb.setMod('state', 'active');
+                    });
                 })
             }
         }
