5eae0eecd3e429b2a6bf9e9d8587f897c8995b58
Guro Bokum <jiojiajiu@gmail.com>
2015-03-15 20:46:42 +0700

    g-item

--- desktop.blocks/g-item/g-item.priv.js
+++ desktop.blocks/g-item/g-item.priv.js
@@ -6,6 +6,10 @@
  *    @key {String} [description]
  *    @key {Array} images
  *      @of {String} url
+ *    @key {Array} [gems]
+ *      @of {Object} gem
+ *        @key {String} name
+ *        @key {String} carat
  *    @key {Array} items
  *      @of {Object} item
  *        @key {Number} price
@@ -21,64 +25,99 @@ blocks['g-item'] = function (data, env) {
     assertHas(data, 'weight', 'Should point weight');
     assertHas(data, 'items', 'Should point items');
 
-    /*
-
-    if (data.item) {
-        console.log(data.item);
-        collumn.content.unshift({
-            block: 'g-item-buy-in-shop',
-            shop: data.item.store,
-            shopUrl: data.item.storeUrl,
-            price: data.item.price,
-            buyUrl: data.item.buyUrl
-        });
-    }
-    */
-
-    return {
+    var block = {
         block: 'g-item',
         content: [{
-            block: 'g-item-heading',
-            mods: { type: 'main' },
+            block: 'g-heading',
             content: data.title
         }, {
             block: 'g-item-category',
             title: data.category
         },
         blocks['g-item-gallery']({ images: data.images }, env),
-        collumn(),{
+        collumn(), {
             block: 'clear'
         }]
-    }
+    };
+
+    return block;
 
     function collumn() {
-        return {}
+        //TODO:
+        var item = data.items[0];
+        //TODO: !sic
+        if (data.description === 'Отстуствует') {
+            data.description = null;
+        }
+
+        var features = {
+            'Артикул': data.number,
+        }
+
+        if (data.weight) {
+            features['Вес'] = data.weight;
+        }
+
+        if (data.gems && data.gems.length) {
+            //TODO: move to lodash
+            var gems = []
+            var carats = []
+            for (var i = 0, length = data.gems.length; i < length; i++) {
+                var gem = data.gems[i];
+
+                if (!(gem.name in gems))
+                    gems.push(gem.name);
+
+                carats.push(gem.carat);
+            }
+            features['Камень'] = gems.join(', ');
+            features['Карат'] = carats.join(', ');
+        }
+
+        if (data.materials && data.materials.length) {
+            var materials = []
+            for (var i = 0, length = data.materials.length; i < length; i++) {
+                var m = data.materials[i];
+                if (!(m in materials))
+                    materials.push(m);
+            }
+            features['Металл'] = materials.join(', ');
+        }
+
         return {
             block: 'g-right-col',
             content: [{
-                    block: 'g-item-description',
-                    content: data.description
-                }, {
-                    block : 'yashare',
-                    quickServices : data.quickServices || [
-                        'vkontakte',
-                        'facebook',
-                        'twitter',
-                        'odnoklassniki'
-                    ],
-                    theme : data.theme || 'counter',
-                    l10n : data.l10n || 'ru',
-                    url : data.url,
-                    title : data.likesTitle,
-                    description : data.likesDescription,
-                    image : 'https://raw.githubusercontent.com/voischev/bem-social/' +
-                            'master/desktop.bundles/index/blocks/page/image/bem.png'
-                }
-            ]
+                block: 'g-item-description',
+                content: data.description
+            }, {
+                block: 'g-item-buy-in-shop',
+                store: item.storeName,
+                url: item.storeUrl,
+                buyUrl: item.buyUrl,
+                price: item.price
+            }, {
+                block: 'g-item-features',
+                content: features
+            }, {
+                block : 'yashare',
+                quickServices : data.quickServices || [
+                    'vkontakte',
+                    'facebook',
+                    'twitter',
+                    'odnoklassniki'
+                ],
+                theme : 'counter',
+                l10n : 'ru',
+                url : data.url,
+                title : data.likesTitle,
+                description : data.likesDescription,
+                image : 'https://raw.githubusercontent.com/voischev/bem-social/' +
+                        'master/desktop.bundles/index/blocks/page/image/bem.png'
+            }]
         };
     }
 }
 
 blocks['g-item.str'] = function (data, env) {
-    return blocks['g-item'](JSON.parse(data));
+    return blocks['g-item'](JSON.parse(data), env);
 }
