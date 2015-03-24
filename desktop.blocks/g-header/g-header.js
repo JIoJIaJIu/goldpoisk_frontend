995777f0f6fd99bafb86a990af64e4058aede983
Yegor Litvyakov <egor@emply.ru>
2015-03-24 13:31:06 +0700

    Issue #103 - another fucking function

--- desktop.blocks/g-header/g-header.js
+++ desktop.blocks/g-header/g-header.js
@@ -5,10 +5,13 @@ modules.define('g-header', ['i-bem__dom', 'jquery'], function(provide, BEMDOM, $
                 $.easing["easeInExpo"] = function (x, t, b, c, d) {
                     return (t==0) ? b : c * Math.pow(2, 10 * (t/d - 1)) + b;
                 }
+                $.easing["easeInQuart"] = function (x, t, b, c, d) {
+                    return c*(t/=d)*t*t*t + b;
+                }
                 var inner = this.findBlockInside('g-header_inner');
                 inner.bindTo('up', 'click', function(e) {
                     console.log('click');
-                    $("body,html").animate({scrollTop: 0}, 750, 'easeInExpo');
+                    $("body,html").animate({scrollTop: 0}, 750, 'easeInQuart');
                 });
 
                 var self = this;
