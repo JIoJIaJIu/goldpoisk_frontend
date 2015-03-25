0b754c6dfa94d372fc7aabff273e964a9db2b6d0
Guro Bokum <jiojiajiu@gmail.com>
2015-03-25 13:26:36 +0700

    Issue #119 Big g-product gallery

--- desktop.blocks/g-item-gallery/g-item-gallery.js
+++ desktop.blocks/g-item-gallery/g-item-gallery.js
@@ -1,22 +1,53 @@
 modules.define('g-item-gallery', ['i-bem__dom', 'jquery', 'slider'], function (provide, BEMDOM, $) {
     BEMDOM.decl('g-item-gallery', {
         onSetMod: {
-            js: function () {
-                var main = this.elem('main_image').find('img');
-                var thumbs = this.findBlocksInside('preview_images', 'image');
-                //TODO: memory leaks
-                thumbs.forEach(function (thumb) {
-                    thumb.bindTo('click', function (e) {
-                        thumbs.forEach(function (t) {
-                            t.delMod('state');
+            js: {
+                'inited': function () {
+                    var main = this.elem('main_image').find('img');
+                    var thumbs = this.findBlocksInside('preview_images', 'image');
+                    var index = 0;
+
+                    //TODO: live event
+                    thumbs.forEach(function (thumb, i) {
+                        thumb.bindTo('click', function (e) {
+                            thumbs.forEach(function (t) {
+                                t.delMod('state');
+                            });
+                            main.attr('src', thumb.params.src);
+                            thumb.setMod('state', 'active');
+                            index = i;
                         });
-                        main.attr('src', thumb.params.src);
-                        thumb.setMod('state', 'active');
                     });
-                })
+
+                    this.bindTo(this.elem('zoom'), 'click', function (e) {
+                        this.__self.getGallery.call(this, index).setMod('showed');
+                    });
+                },
+
+                '': function () {
+                    //Finalization
+                }
             }
         }
-    }, {});
+    }, {
+        getGallery: function (index) {
+            //TODO: recover cache
+            //TODO: good destructing?
+            if (this.__self.gallery) {
+                BEMDOM.destruct(this.__self.gallery.domElem);
+            }
+
+            var html = BEMHTML.apply({
+                block: 'g-gallery',
+                index: index,
+                images: this.params.images
+            });
+
+            var dom = BEMDOM.append(document.body, html);
+            this.__self.gallery = this.findBlockOn(dom, 'g-gallery');
+            return this.__self.gallery;
+        }
+    });
 
     provide(BEMDOM);
 });
