054bf39189f888072a01ff644a4d1de3ce6263da
Yegor Litvyakov <egor@emply.ru>
2015-03-28 11:28:56 +0700

    js технология для g-menu-item

---
+++ min.blocks/g-menu-item/g-menu-item.js
@@ -0,0 +1,10 @@
+modules.define('g-menu-item', ['i-bem__don', 'router'], function(provide, BEMDOM, router){
+    BEMDOM.decl('g-menu-item', {
+        onSetMod: {
+            'js': function () {
+
+            }
+        }
+    }, {});
+    provide(BEMDOM);
+});
