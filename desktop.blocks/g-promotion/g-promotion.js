bada7bd68596a3fd8b2c9f90c40f824d1814dd7e
Yegor Litvyakov <egor@emply.ru>
2015-03-28 11:16:59 +0700

    g-promotion destruct js

--- desktop.blocks/g-promotion/g-promotion.js
+++ desktop.blocks/g-promotion/g-promotion.js
@@ -2,45 +2,50 @@ modules.define('g-promotion', ['i-bem__dom', 'slider'], function (provide, BEMDO
 
 BEMDOM.decl('g-promotion', {
     onSetMod: {
-        js: function () {
-            var transitions = [{
-                $Duration: 1800,
-                x: 1,
-                $Delay: 30,
-                $Cols: 10,
-                $Rows: 5,
-                $Clip: 15,
-                $During: {
-                    $Left: [0.3,0.7]
-                },
-                $Formation: $JssorSlideshowFormations$.$FormationStraightStairs,
-                $Assembly: 260,
-                $Easing: {
-                    $Left: $JssorEasing$.$EaseInOutExpo,
-                    $Clip: $JssorEasing$.$EaseInOutQuad
-                },
-                $Round: {
-                    $Top: 0.8
-                }
-            }];
+        js: {
+            inited: function () {
+                var transitions = [{
+                    $Duration: 1800,
+                    x: 1,
+                    $Delay: 30,
+                    $Cols: 10,
+                    $Rows: 5,
+                    $Clip: 15,
+                    $During: {
+                        $Left: [0.3,0.7]
+                    },
+                    $Formation: $JssorSlideshowFormations$.$FormationStraightStairs,
+                    $Assembly: 260,
+                    $Easing: {
+                        $Left: $JssorEasing$.$EaseInOutExpo,
+                        $Clip: $JssorEasing$.$EaseInOutQuad
+                    },
+                    $Round: {
+                        $Top: 0.8
+                    }
+                }];
 
-            var slider = this.slider = new $JssorSlider$(this.elem('inner')[0], {
-                $AutoPlay: true,
-                $ArrowKeyNavigation: true,
-                $SlideshowOptions: {
-                    $Class: $JssorSlideshowRunner$,
-                    $Transitions: transitions,
-                    $TransitionsOrder: 1,
-                    $ShowLink: true
-                }
-            });
+                var slider = this.slider = new $JssorSlider$(this.elem('inner')[0], {
+                    $AutoPlay: true,
+                    $ArrowKeyNavigation: true,
+                    $SlideshowOptions: {
+                        $Class: $JssorSlideshowRunner$,
+                        $Transitions: transitions,
+                        $TransitionsOrder: 1,
+                        $ShowLink: true
+                    }
+                });
 
-            var markers = this.findBlocksInside('g-promotion-marker');
-            slider.$On($JssorSlider$.$EVT_PARK, function (to, from) {
-                if (~from)
-                    markers[from].delMod('state');
-                markers[to].setMod('state', 'selected');
-            });
+                var markers = this.findBlocksInside('g-promotion-marker');
+                slider.$On($JssorSlider$.$EVT_PARK, function (to, from) {
+                    if (~from)
+                        markers[from].delMod('state');
+                    markers[to].setMod('state', 'selected');
+                });
+            }
+        },
+        '': function () {
+            this.slider = null;
         }
     },
 
