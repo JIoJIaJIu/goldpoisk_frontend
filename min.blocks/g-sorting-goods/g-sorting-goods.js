42d1a866d1a56b1827961e18220e94c45296c640
Yegor Litvyakov <egor@emply.ru>
2015-03-29 19:52:14 +0700

    Issue #56 - Затемнение елемента контент при сортировке

--- min.blocks/g-sorting-goods/g-sorting-goods.js
+++ min.blocks/g-sorting-goods/g-sorting-goods.js
@@ -1,18 +1,21 @@
 modules.define('g-sorting-goods', ['i-bem__dom', 'jquery'], function(provide, BEMDOM, $) {
     BEMDOM.decl('g-sorting-goods', {
         _onParamClick: function(e) {
+            var that = this;
             var goods = this.findBlockOutside('g-content').findBlockInside('g-goods');
             var _target = $(e.currentTarget);
             this.setMod(this.elem('param'), 'selected', false);
             this.setMod(_target, 'selected', true);
-            goods.setMod('loading', true);
+            this.setMod('loading', true);
+            goods.setMod(goods.elem('content'), 'loading', true);
             //TODO:
             var url = $(e.target).data('bem')['g-sorting-goods__param'].url;
 
             $.getJSON(url, function sort (data) {
                 setTimeout(function () {
                     goods.update(data);
-                    goods.setMod('loading', false);
+                    goods.setMod(goods.elem('content'), 'loading', false);
+                    that.setMod('loading', false);
                 }, 3000);
             })
         }
