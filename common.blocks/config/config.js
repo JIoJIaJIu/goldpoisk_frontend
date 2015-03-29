409c2fa9d3cb7618937558898471c5ddd22e9fcd
Yegor Litvyakov <egor@emply.ru>
2015-03-29 11:29:01 +0700

    Issue #143 - файл конфигурации с url'ами и priv'ами

--- common.blocks/config/config.js
+++ common.blocks/config/config.js
@@ -8,6 +8,48 @@ modules.define('config', ['i-bem'], function (provide) {
             rings: {
                 list: 'http://localhost:3000/success'
             }
+        },
+        page: {
+            index: {
+                url: '/',
+                priv: 'index.content'
+            },
+            necklaces: {
+                url: '/necklaces',
+                priv: 'category.content'
+            },
+            chains: {
+                url: '/chains',
+                priv: 'category.content'
+            },
+            pendants: {
+                url: '/pendants',
+                priv: 'category.content'
+            },
+            bracelets: {
+                url: '/bracelets',
+                priv: 'category.content'
+            },
+            rings: {
+                url: '/rings',
+                priv: 'category.content'
+            },
+            earrings: {
+                url: '/earrings',
+                priv: 'category.content'
+            },
+            brooches: {
+                url: '/brooches',
+                priv: 'category.content'
+            },
+            watches: {
+                url: '/watches',
+                priv: 'category.content'
+            },
+            cutlery: {
+                url: '/cutlery',
+                priv: 'category.content'
+            }
         }
     })
 });
