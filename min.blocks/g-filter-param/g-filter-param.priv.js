c34b9f95c58d2d518170132e6341c65acd7ea467
Yegor Litvyakov <egor@emply.ru>
2015-03-30 16:37:05 +0700

    Issue #96 - priv и bemhtml блока g-filter-param

---
+++ min.blocks/g-filter-param/g-filter-param.priv.js
@@ -0,0 +1,39 @@
+/**
+ * @param {Object} data
+ *  @key {String} title
+ *  @key {String} type
+ * if type == list
+ *  @key {String} more like 'Ещё 5 материалов'
+ *  @key {String} state open || closed
+ *  @key {Array} values
+ *   @of {String} value like 'Золото'
+ * if type == price
+ *  @key {Integer} min
+ *  @key {Integer} max
+ *  @key {Integer} scale
+ *  @key {Array} ticks
+ *   @of {Integer} tick like 2100
+ */
+blocks['g-filter-param'] = function (data, env) {
+    switch (data.type) {
+        case 'list':        
+            return {
+                block: 'g-filter-param',
+                mods: { 'list': true, 'state': data.state},
+                title: data.title,
+                more: data.more,
+                content: data.values
+            }
+            break;
+        case 'price':
+            return {
+                block: 'g-filter-param',
+                title: data.title,
+                min: data.min,
+                max: data.max,
+                scale: data.scale,
+                ticks: data.ticks
+            }
+            break;
+    }
+}
