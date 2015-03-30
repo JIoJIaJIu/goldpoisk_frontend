974dc2b6ec5e6b97def6e8bef81911c6aa735cba
Yegor Litvyakov <egor@emply.ru>
2015-03-30 16:37:38 +0700

    Issue #96 - priv блока g-filter

---
+++ desktop.blocks/g-filter/g-filter.priv.js
@@ -0,0 +1,40 @@
+/**
+ * @param {Object} data
+ *  @key {Array} params
+ *   @of {Object} param
+ *    @key {String} title
+ *    @key {String} type
+ *     if type == list
+ *    @key {String} more like 'Ещё 5 материалов'
+ *    @key {String} state open || closed
+ *    @key {Array} values
+ *    @of {String} value like 'Золото'
+ *     if type == price
+ *    @key {Integer} min
+ *    @key {Integer} max
+ *    @key {Integer} scale
+ *    @key {Array} ticks
+ *    @of {Integer} tick like 2100
+ */
+blocks['g-filter'] = function (data, env) {
+    var params = [];
+    data.params.map(function (param) {
+        params.push(blocks['g-filter-param'](param));
+    });
+    return {
+        block: 'g-filter',
+        mods: { 'hidden': false, },
+        tag: 'section',
+        content: [{
+                elem: 'title',
+                tag: 'h1',
+                content: 'Поиск по параметрам'
+            }, {
+                elem: 'list',
+                content: params
+            }, {
+                elem: 'button'
+            }
+        ]
+    }
+}
