207705c3a933d0d5c6ad8e8c86e5b60c6f79cd62
Yegor Litvyakov <egor@emply.ru>
2015-03-30 14:30:36 +0700

    Issue #96 - JS блока фильтров с фоновой подложкой для фильтров

--- desktop.blocks/g-filter/g-filter.js
+++ desktop.blocks/g-filter/g-filter.js
@@ -1,6 +1,13 @@
-modules.define('g-filter', ['i-bem__dom'], function(provide, BEMDOM) {
+modules.define('g-filter', ['i-bem__dom', 'jquery'], function(provide, BEMDOM, $) {
     BEMDOM.decl('g-filter', {
-
+        onSetMod: {
+            'js': function () {
+                this.bindTo(this.elem('button'), 'click', function (e) {
+                    console.log('click button');
+                    this.toggleMod('hidden');
+                });
+            }
+        }
     }, {});
     provide(BEMDOM);
 })
