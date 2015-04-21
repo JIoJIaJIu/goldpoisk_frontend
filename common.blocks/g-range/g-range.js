e8ee2a668fe570457e818384b32c40dd6acfbe01
Yegor Litvyakov <egor@emply.ru>
2015-04-21 16:32:23 +0700

    Issue #195 - js для правого бегунка

--- common.blocks/g-range/g-range.js
+++ common.blocks/g-range/g-range.js
@@ -8,17 +8,36 @@ modules.define('g-range', ['i-bem__dom'], function(provide, BEMDOM) {
                     var rightThumb = this.elem('thumb', 'right', true);
                     var leftThumb = this.elem('thumb', 'left', true);
                     var handlebar = this.elem('handlebar');
+                    var handlebarStart = handlebar.offset().left;
+                    var handlebarWidth = handlebar.outerWidth() - 1;
                     var fill = this.elem('fill');
-                    this.bindTo(rightThumb, 'click', function (e) {
+                    var fillStart = fill.offset().left - handlebarStart;
+                    var fillWidth = fill.width();
+                    var fillEnd = fillStart + fillWidth;
+                    this.bindTo(rightThumb, 'mousedown', function (e) {
                         e.stopPropagation();
+                        //@TODO избавиться от скачка при mouseup
+                        $(document).bind('mousemove', function (e) {
+                            var percent = (e.pageX - handlebar.offset().left) * 100 / handlebar.outerWidth() - 1;
+                            if ((e.pageX - handlebar.offset().left) < (fill.offset().left - handlebar.offset().left)) {
+
+                            } else {
+                                fill.css('margin-right', 100 - percent + '%');
+                            }
+                        });
+                        $(document).bind('mouseup', function (e) {
+                            console.log('key up');
+                            $(document).unbind('mousemove');
+                            $(document).unbind('mouseup');
+                        });
                     });
                     this.bindTo(handlebar, 'click', function (e) {
-                        var handlebarStart = handlebar.offset().left;
-                        var handlebarWidth = handlebar.outerWidth() - 1;
-                        var clickPoint = e.pageX - handlebarStart;
-                        var fillStart = fill.offset().left - handlebarStart;
-                        var fillWidth = fill.width();
-                        var fillEnd = fillStart + fillWidth;
+                        handlebarStart = handlebar.offset().left;
+                        handlebarWidth = handlebar.outerWidth() - 1;
+                        clickPoint = e.pageX - handlebarStart;
+                        fillStart = fill.offset().left - handlebarStart;
+                        fillWidth = fill.width();
+                        fillEnd = fillStart + fillWidth;
                         if (clickPoint >= 0 && clickPoint <= handlebarWidth) {
                             var percent = clickPoint * 100 / handlebarWidth;
                             if ((Math.abs(clickPoint - fillStart)) < (Math.abs(clickPoint - fillEnd))) {
