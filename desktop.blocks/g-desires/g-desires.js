38eff40150d3853d50803097ecc84063423929d0
Yegor Litvyakov <egor@emply.ru>
2015-04-22 22:50:49 +0700

    Issue #252 Загрузка данных с сервера в модальное окно желаний

--- desktop.blocks/g-desires/g-desires.js
+++ desktop.blocks/g-desires/g-desires.js
@@ -13,7 +13,7 @@ modules.define('g-desires', ['i-bem__dom', 'cookie'], function(provide, BEMDOM, 
                     });
                     if (this._list.length) {
                         if (this.hasMod('empty'))
-                            delMod('empty');
+                            this.delMod('empty');
                         this.recount();
                     } else {
                         this.setMod('empty', true);
@@ -24,69 +24,33 @@ modules.define('g-desires', ['i-bem__dom', 'cookie'], function(provide, BEMDOM, 
                     link.bindTo('click', function (e) {
                         e.preventDefault();
                         var header = self.findBlockOutside('g-header');
-                        var content = {
-                            block: 'g-modal',
-                            content: [{
-                                block: 'g-heading',
-                                mods: { dark: true, size: 'l' },
-                                count: 11,
-                                content: 'Список желаний'
-                            }, {
-                                block: 'g-description',
-                                tag: 'p',
-                                content: 'Понравившиеся вам товары хранятся здесь, и ждут когда вы их купите. Мы пришлем вам весточку, если на выбранные товары появятся скидки.'
-                            }, {
-                                block: 'g-goods',
+                        var products;
+                        $.getJSON('/sortparam', self._list.join('.'), function success(data) {
+                            debugger;
+                            products = blocks['g-goods.items'](data);
+                            var content = {
+                                block: 'g-modal',
                                 content: [{
-                                    block: 'g-product',
-                                    mods: { like: true, action: true },
-                                    url: '#',
-                                    title: 'Золотое Кольцо с гранатами и фианитами. Очень красивое и неебически дорогое',
-                                    imageUrl: '../../desktop.blocks/g-goods/images/good.png',
-                                    weight: '4.5 грамм',
-                                    carat: '0,07 карат',
-                                    price: 1000000,
-                                    store: 'Sunlight',
-                                    storeUrl: '#'
+                                    block: 'g-heading',
+                                    mods: { dark: true, size: 'l' },
+                                    count: self._list.length,
+                                    content: 'Список желаний'
                                 }, {
-                                    block: 'g-product',
-                                    mods: { like: true, hit: true },
-                                    url: '#',
-                                    title: 'Золотое Кольцо с гранатами и фианитами. Очень красивое и неебически дорогое',
-                                    imageUrl: '../../desktop.blocks/g-goods/images/good.png',
-                                    weight: '4.5 грамм',
-                                    carat: '0,07 карат',
-                                    price: 1000000,
-                                    store: 'Sunlight',
-                                    storeUrl: '#'
+                                    block: 'g-description',
+                                    tag: 'p',
+                                    content: 'Понравившиеся вам товары хранятся здесь, и ждут когда вы их купите. Мы пришлем вам весточку, если на выбранные товары появятся скидки.'
                                 }, {
-                                    block: 'g-product',
-                                    url: '#',
-                                    title: 'Золотое Кольцо с гранатами и фианитами. Очень красивое и неебически дорогое',
-                                    imageUrl: '../../desktop.blocks/g-goods/images/good.png',
-                                    weight: '4.5 грамм',
-                                    carat: '0,07 карат',
-                                    price: 1000000,
-                                    store: 'Sunlight',
-                                    storeUrl: '#'
-                                }, {
-                                    block: 'g-product',
-                                    mods: { like: false, action: true },
-                                    url: '#',
-                                    title: 'Золотое Кольцо с гранатами и фианитами. Очень красивое и неебически дорогое',
-                                    imageUrl: '../../desktop.blocks/g-goods/images/good.png',
-                                    weight: '4.5 грамм',
-                                    carat: '0,07 карат',
-                                    price: 1000000,
-                                    store: 'Sunlight',
-                                    storeUrl: '#'
+                                    block: 'g-goods',
+                                    content: [
+                                        products
+                                    ]
                                 }]
-                            }]
-                        };
-                        BEMDOM.before(
-                            header.domElem,
-                            BEMHTML.apply(content)
-                        );
+                            };
+                            BEMDOM.before(
+                                header.domElem,
+                                BEMHTML.apply(content)
+                            );
+                        });
                     });
                 },
                 '': function () {
