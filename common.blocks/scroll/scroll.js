ded47d89f61ebdc479567ba7c486451e656bf448
Yegor Litvyakov <egor@emply.ru>
2015-05-18 14:01:26 +0700

    Правки по замечаниям

--- common.blocks/scroll/scroll.js
+++ common.blocks/scroll/scroll.js
@@ -1,32 +1,25 @@
 modules.define('scroll', ['i-bem__dom', 'inherit', 'events'], function (provide, BEMDOM, inherit, events) {
     
     var scroll = inherit(events.Emitter, {
-        __constructor: function () {console.log('init scroll')},
-        init: function () {
-            if (this._isInited)
-                return;
+        __constructor: function () {
             var self = this;
-            this._inited = true;
-            var prevTop = 0;
-            this.scrollDefFn = function (e) {
+            var top = 0;
+
+            $(window).bind('scroll', function (e) {
                 var currentTop = $(window).scrollTop();
-                var diff = prevTop - currentTop;
+                var diff = top - currentTop;
+
                 if (diff > 0) {
                     self.emit('scrollTop');
                 } else if (diff < 0) {
                     self.emit('scrollBottom');
                 } else if (diff == 0)
                     return;
-                prevTop = currentTop;
+
+                top = currentTop;
                 self.emit('scroll');
-            }
-            $(window).bind('scroll', this.scrollDefFn);
-            this._isInited = true;
-        },
-        finalize: function () {
-            $(window).unbind('scroll', this.scrollDefFn);
-        },
-        _isInited: false
+            });
+        }
     });
 
     provide(new scroll());
