fc3b64e14ba8364bb0b329629b4a23072a12ab75
Yegor Litvyakov <egor@emply.ru>
2015-05-27 17:21:15 +0700

    Issue #301 [Улучшение] Сделать заглушку для g-gallery Изобрежение заглушки заменено на аналог в формате svg Прописаны заглушки для маленьких и больших картинок в галерее P.S. @JIoJIaJIi проверь работоспособность полноэкранной галереи

--- desktop.blocks/g-item-gallery/g-item-gallery.priv.js
+++ desktop.blocks/g-item-gallery/g-item-gallery.priv.js
@@ -1,36 +1,37 @@
 blocks['g-item-gallery'] = function (data, env) {
-	/**
+    /**
      *  @param {Object} data
-	 *    @key {Array} images
-	 *    @key {Number} [index] default = 0
+     *    @key {Array} images
+     *    @key {Number} [index] default = 0
      *    @key {String} alt
      *
      *  @param {Object} [env]
      *    @key {Boolean} big, set modificator 'big'
-	 **/
+     **/
      env = env || {};
 
-	 var MAX_IMG_COUNTS = 4;
+     var MAX_IMG_COUNTS = 4;
      var index = data.index || 0;
-	 var block = {
-	 	block: 'g-item-gallery',
+     var block = {
+        block: 'g-item-gallery',
         images: data.images,
-	 	content: [{
-	 			elem: 'title',
-	 			title: 'Фото',
-	 			current: index,
-	 			count: (data.images.length > MAX_IMG_COUNTS) ? MAX_IMG_COUNTS : data.images.length
-	 		}, {
-	 			elem: 'preview_images',
+        content: [{
+                elem: 'title',
+                title: 'Фото',
+                current: index,
+                count: (data.images.length > MAX_IMG_COUNTS) ? MAX_IMG_COUNTS : data.images.length
+            }, {
+                elem: 'preview_images',
                 alt: data.alt,
-	 			images: (data.images.length > MAX_IMG_COUNTS) ? data.images.slice(0, MAX_IMG_COUNTS) : data.images
-	 		}, {
-	 			elem: 'main_image',
+                images: (data.images.length > MAX_IMG_COUNTS) ? data.images.slice(0, MAX_IMG_COUNTS) : data.images
+            }, {
+                elem: 'main_image',
+                big: env.big,
                 alt: data.alt,
-	 			image: data.images[index]
-	 		}
-	 	]
-	 }
+                image: data.images[index]
+            }
+        ]
+     }
 
      if (env.big) {
         block.mods = { type: 'big' };
